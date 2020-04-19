670cfb5ac19062c97b1f12ccbd160ea036c9ecd9
Chris Kerr <chriskerr@me.com>
2020-04-20 08:31:52 +1000

    removed pimontior for github

--- src/router/index.js
+++ src/router/index.js
@@ -18,11 +18,11 @@ const routes = [
 		// which is lazy-loaded when the route is visited.
 		component: () => import( /* webpackChunkName: "about" */ "../views/About.vue" ),
 	},
-	{
-		path: "/pimonitor",
-		name: "pimonitor",
-		component: () => import( "../views/Pimonitor.vue" ),
-	},
+	// {
+	// 	path: "/pimonitor",
+	// 	name: "pimonitor",
+	// 	component: () => import( "../views/Pimonitor.vue" ),
+	// },
 	{
 		path: "/projects",
 		name: "projects",
